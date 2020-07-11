695289bdb0ca6cdc0e6c64c3e186bc1407de150a
David <learnitmyway@users.noreply.github.com>
2020-07-11 18:41:06 +0200

    get urls

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const getURLs = require('./url.service')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', async (req, res) => {
+  const URLs = await getURLs()
+  res.send(URLs)
+})
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ url.service.js
@@ -0,0 +1,32 @@
+var AWS = require('aws-sdk')
+
+async function getURLs() {
+  AWS.config.update({
+    region: 'us-west-2',
+    endpoint: 'http://localhost:8000',
+  })
+
+  var docClient = new AWS.DynamoDB.DocumentClient()
+
+  var params = {
+    TableName: 'URLs',
+  }
+
+  const data = await docClient
+    .scan(params, (err, data) => {
+      if (err) {
+        console.error('Unable to query. Error:', JSON.stringify(err, null, 2))
+        throw err
+      } else {
+        console.log('Query succeeded.')
+        data.Items.forEach(({ hash, originalURL }) => {
+          console.log(' -', hash + ': ' + originalURL)
+        })
+      }
+    })
+    .promise()
+
+  return data.Items
+}
+
+module.exports = getURLs
