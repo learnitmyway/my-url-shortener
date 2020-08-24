b5dd9f5d7c7935ab8378507ccd16b93c1d28f41b
David <learnitmyway@users.noreply.github.com>
2020-08-24 19:57:28 +0200

    store hash in db

--- src/url.service.js
+++ src/url.service.js
@@ -1,19 +1,27 @@
 const AWS = require('aws-sdk')
 const md5 = require('md5')
 
-async function getURLs() {
+const tableName = 'URLs'
+
+function updateAwsConfig() {
   AWS.config.update({
     region: 'us-west-2',
     endpoint: 'http://localhost:8000',
   })
+}
 
-  var docClient = new AWS.DynamoDB.DocumentClient()
+function instantiateDocClient() {
+  return new AWS.DynamoDB.DocumentClient()
+}
+
+async function getURLs() {
+  updateAwsConfig()
 
   var params = {
-    TableName: 'URLs',
+    TableName: tableName,
   }
 
-  const data = await docClient
+  const data = await instantiateDocClient()
     .scan(params, (err, data) => {
       if (err) {
         console.error('Unable to query. Error:', JSON.stringify(err, null, 2))
@@ -30,10 +38,39 @@ async function getURLs() {
   return data.Items
 }
 
-async function createUrl(originalUrl) {
-  console.log('createUrl originalUrl', originalUrl)
-  const hash = Buffer.from(md5(originalUrl)).toString('base64').substring(0, 5)
-  return 'www.my-url-shortener.com/' + hash
+async function createUrl(originalURL) {
+  console.log('createUrl originalUrl', originalURL)
+  updateAwsConfig()
+
+  const hash = md5(originalURL)
+  const newUrl = {
+    hash,
+    originalURL,
+    creationDate: Date.now().toString(),
+    expirationDate: Date.now().toString(),
+    userId: '3',
+  }
+  const params = {
+    TableName: tableName,
+    Item: newUrl,
+  }
+
+  instantiateDocClient()
+    .put(params, (err) => {
+      if (err) {
+        console.error(
+          'Unable to add URL',
+          originalURL,
+          '. Error JSON:',
+          JSON.stringify(err, null, 2)
+        )
+      } else {
+        console.log('PutItem succeeded:', originalURL)
+      }
+    })
+    .promise()
+  const encoded = Buffer.from(hash).toString('base64').substring(0, 5)
+  return 'www.my-url-shortener.com/' + encoded
 }
 
 module.exports = { getURLs, createUrl }
