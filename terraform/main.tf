b82518e9dde517e17d84bcf9fbbb1b6a64fa0cfe
David <learnitmyway@users.noreply.github.com>
2020-08-26 18:03:48 +0200

    add Slugs table

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,14 @@ resource "aws_dynamodb_table" "urls-table" {
     type = "S"
   }
 }
+
+resource "aws_dynamodb_table" "slugs-table" {
+  name         = "Slugs"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "slug"
+
+  attribute {
+    name = "slug"
+    type = "S"
+  }
+}
